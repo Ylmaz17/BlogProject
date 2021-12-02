cc7ba1551286080fd42bd1658e47ddd264658c21
İsmail Yılmaz <iylmz1707@gmail.com>
2021-12-02 17:52:56 +0300

    42.Ders Sonu

---
+++ BusinessLayer/Concrete/ContactManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ContactManager : IContactService
+    {
+        IContactDal _contectDal;
+
+        public ContactManager(IContactDal contectDal)
+        {
+            _contectDal = contectDal;
+        }
+
+        public void ContactAdd(Contact contact)
+        {
+            _contectDal.Insert(contact);
+        }
+    }
+}
